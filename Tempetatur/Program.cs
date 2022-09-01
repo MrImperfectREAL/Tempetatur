5d00273686062ed842a41dea29e731495f76ffe1
brend <brend@DESKTOP-36ES5SN>
2022-09-01 10:45:29 +0200

    Add project files.

---
+++ Tempetatur/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tempetatur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tast inn temperaturen");
+
+            int temperatur = int.Parse(Console.ReadLine());
+
+            if (temperatur > 0)
+
+            {
+
+                Console.WriteLine("Temperaturen er varmegrader");
+
+            }
+
+            else
+
+            {
+
+                Console.WriteLine("Temperaturen er kalde grader");
+
+            }
+        }
+    }
+}
